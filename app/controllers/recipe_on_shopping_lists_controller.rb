ee709e252fd8e7b743ca1be018dc051629a0cae6
marklc44 <marklc44@gmail.com>
2013-12-17 11:33:15 -0800

    finally fixed recipes_on_shopping_lists_controller! (knock wood)

--- app/controllers/recipe_on_shopping_lists_controller.rb
+++ app/controllers/recipe_on_shopping_lists_controller.rb
@@ -1,8 +1,14 @@
 class RecipeOnShoppingListsController < ApplicationController
-	def create
-		recipe_on_shopping_list = RecipeOnShoppingList.new
-		recipe_on_shopping_list.shopping_list = ShoppingList.find(params[:shopping_list_id])
-		recipe_on_shopping_list.recipe = Recipe.where(:yummly_id => params[:yummly_id]).first
-		# recipe_on_shopping_list.save
-	end
+    def create
+        recipe_on_shopping_list = RecipeOnShoppingList.new
+        recipe_on_shopping_list.shopping_list = ShoppingList.find(params[:shopping_list_id])
+        recipe_on_shopping_list.recipe = Recipe.where(:yummly_id => params[:yummly_id]).first
+        recipe_on_shopping_list.save
+
+        if recipe_on_shopping_list.save
+	        redirect_to root_path
+	    else
+	    	render :new
+	    end
+    end
 end
